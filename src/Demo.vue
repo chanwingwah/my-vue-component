7662f1a615ea4798b77433eac8f2d26003e4c67d
chenyh <917029221@qq.com>
2021-03-11 08:11:31 +0800

    项目目录结构整理

---
+++ src/Demo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    HelloWorld
+  </div>
+</template>
+<script>
+export default {
+  name: "Demo",
+  components: {}
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
