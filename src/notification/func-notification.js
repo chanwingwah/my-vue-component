8e03e37099766127372fb7012d738c07cfd424a9
chenyh <917029221@qq.com>
2021-03-14 05:47:00 +0800

    Notification:  优化关闭逻辑,从dom 中清除

--- src/notification/func-notification.js
+++ src/notification/func-notification.js
@@ -36,7 +36,7 @@ export default {
   methods: {
     createTimer(time) {
       this.timer = setTimeout(() => {
-        this.visible = false;
+        this.handleClose();
       }, time);
     },
     clearTimer() {
--- src/notification/function.js
+++ src/notification/function.js
@@ -3,6 +3,12 @@ import Notification from "./func-notification";
 
 const NotificationConstructor = Vue.extend(Notification);
 
+const removeInstance = instance => {
+  if (!instance) return;
+  document.body.removeChild(instance.$el);
+  instance.$destroy();
+};
+
 const notify = propsData => {
   const instance = new NotificationConstructor({
     propsData
@@ -12,7 +18,8 @@ const notify = propsData => {
   document.body.appendChild(instance.$el);
   // 绑定关闭按钮事件
   instance.$on("close", () => {
-    instance.visible = false;
+    // instance.visible = false;
+    removeInstance(instance);
   });
   return instance;
 };
